408a5f470dab4727c34e24cf8b859ad66a23687a
Leonardo Maglia <leonardo.maglia@fatec.sp.gov.br>
2019-06-08 02:25:37 -0300

    Criada tela de login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <div class="center-screen">
+          <form class="form-signin">
+            <img src={ require('./images/logo.png') } />
+            <h1 class="h3 mb-3 font-weight-normal">MEETING MANAGER</h1>
+
+            <input type="email" id="inputEmail" class="form-control" placeholder="E-mail" required autofocus/>
+            <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required/>
+
+            <div class="checkbox mb-3">
+              <label>
+                <input type="checkbox" value="remember-me"/> Lembrar-me
+              </label>
+            </div>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+          </form>
+        </div>
+
     </div>
   );
 }
